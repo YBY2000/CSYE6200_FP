2107baec612d0c5931caa67704868dd30c4c2b1b
YBY2000 <ye.boy@northeastern.edu>
2023-12-11 21:46:01 -0500

    fix: :bug: prevent user from getting into other page without login

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -8,6 +8,16 @@ const router = createRouter({
       name: 'Manager',
       component: () => import('@/views/Manager.vue'),
       redirect: '/home',
+      beforeEnter: (to, from, next) => {
+        const user = JSON.parse(localStorage.getItem('student-user') || '{}')
+        if (!user || !user.username) {
+          // User is not logged in, redirect to login page or other appropriate route
+          next('/login') // Replace '/login' with the route you want to redirect to
+        } else {
+          // User is logged in, proceed to the requested route
+          next()
+        }
+      },
       children: [
         { path: 'home', name: 'Home', component: () => import('@/views/manager/Home.vue')},
         { path: 'course', name: 'Course', component: () => import('@/views/manager/Course.vue')},
